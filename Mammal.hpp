6a4a5ff972a888918bd7f96432e36e7133f49f2e
Sophie Ong <48041705+Sophalopha@users.noreply.github.com>
2020-02-15 20:49:43 -0500

    Update Mammal.hpp

--- Mammal.hpp
+++ Mammal.hpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #ifndef MAMMAL_HPP // include guard
 #define MAMMAL_HPP
+#include "Animal.hpp"
 
 //DERIVED CLASS
 
 class Mammal: public Animal 
 {
     public:
-    Mammal();
-    Mammal(std::string name, bool domestic = false, bool predator = false); 
-    bool hasHair() const;
-    bool isAirborne() const;
-    bool isAquatic() const; 
-    bool isToothed() const;
-    bool hasFins() const; 
-    bool hasTail() const;
-    int legs() const;
-    void setHair();
-    void setAirborne();
-    void setAquatic(); 
-    void setToothed();
-    void setFins(); 
-    void setTail();
-    void setLegs(int legs);
+        Mammal();
+        Mammal(std::string name, bool domestic = false, bool predator = false); 
+        bool hasHair() const;
+        bool isAirborne() const;
+        bool isAquatic() const; 
+        bool isToothed() const;
+        bool hasFins() const; 
+        bool hasTail() const;
+        int legs() const;
+        void setHair();
+        void setAirborne();
+        void setAquatic(); 
+        void setToothed();
+        void setFins(); 
+        void setTail();
+        void setLegs(int legs);
 
     private:
-    bool hair_;
-    bool airborne_;
-    bool aquatic_; 
-    bool toothed_;
-    bool fins_; 
-    bool tail_;
-    int legs_;
+        bool hair_;
+        bool airborne_;
+        bool aquatic_; 
+        bool toothed_;
+        bool fins_; 
+        bool tail_;
+        int legs_;
  
 };
 
@@ -39,5 +40,6 @@ class Mammal: public Animal
 
 
 #endif // MAMMAL_HPP
+
 
 
